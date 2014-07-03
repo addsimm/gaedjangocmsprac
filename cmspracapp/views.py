d4908dfa8c34fb7f5c030006dbf96f4b8c9ba10a
adamsimon <adamfsimon@gmail.com>
2014-07-03 09:07:27 -0700

    Added home from laptop

--- cmspracapp/views.py
+++ cmspracapp/views.py
@@ -1,1 +1,13 @@
 # Create your views here.
+
+from django.http import HttpResponse
+from django import template
+from django.http import HttpResponseRedirect
+from django.shortcuts import render_to_response
+import datetime
+
+def home(request):
+    now = datetime.datetime.now()
+    return render_to_response('home.html',
+                              {'now': now,}
+    )
